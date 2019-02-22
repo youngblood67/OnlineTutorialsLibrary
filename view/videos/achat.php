fa5347480515f718de56a98ef0bd6a388714d241
Philippe Schaeffer <philippeschaeffer67@gmail.com>
2019-02-22 18:33:52 +0100

    commit après modif dans gitignore

---
+++ view/videos/achat.php
@@ -0,0 +1,25 @@
+<div class="container-fluid text-center">
+    <div class="jumbotron">
+        <div id="buy-list">
+
+            <h4>Confirmation de l'achat des vidéos</h4>
+            <ul id="videos-buy-list">
+
+            </ul>
+        </div>
+        <div id="total-achat"><span id="span-total-achat">TOTAL : <span id="total"></span> €</span>
+
+
+            <div id="grp-btn-achat">
+
+
+                <?php require "../view/videos/paypalButton.php"?>
+
+
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
